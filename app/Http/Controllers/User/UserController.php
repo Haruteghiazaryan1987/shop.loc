bfd8c8be22e9e06b41df0b0e8e9652951cfceb16
Harut <harut666180666@gmail.com>
2021-03-01 17:48:40 +0400

    formating

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,17 +5,13 @@ namespace App\Http\Controllers\User;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 
-class UserController extends Controller
-{
-    public function __construct()
-    {
+class UserController extends Controller {
+    public function __construct() {
         $this->middleware('auth');
     }
 
-    public function index()
-    {
+    public function index() {
         $products = Product::get();
-        // dd($products);
         return view('user.home.index', compact('products'));
     }
 }
